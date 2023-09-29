2cdc1f811b22ebfaca04ec132c7dbb586f99f73b
Tayyab16203 <rehmantayyab0340@gmail.com>
2023-09-29 12:47:02 -0700

    ini

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    {/* <Navbar title = "Textutils" aboutText= "About Textutils"/> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils"/>
-    <div className='container my-3'>
-      <TextForm heading = "Enter the text to analize below"/>
-    </div>
+      {/* <Navbar title = "Textutils" aboutText= "About Textutils"/> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils"/>
+      <div className='container my-3'>
+        <TextForm heading = "Enter the text to analize below"/>
+        {/* <About/> */}
+      </div>
     </>
   );
 }
--- React/my-app/src/components/TextForm.js
+++ React/my-app/src/components/TextForm.js
@@ -1,27 +1,50 @@
 import React,{useState} from 'react'
 
-
 export default function TextForm(props) {
     const handleUpClick = () =>{
         // console.log("Uppercase was Clicked" + text);
         let newtext = text.toUpperCase();
         settext(newtext)
     }
+     const handleLoClick = () =>{
+        let newtext = text.toLowerCase();
+        settext(newtext)
+    }
+    const handleclearClick = () =>{
+        settext("")
+    }
+    const handleCopy = () =>{
+        let cotext = document.getElementById("myBox");
+        cotext.select();
+        navigator.clipboard.writeText(cotext.value);
+    }
     const handleOnChange = (event) =>{
         console.log("On change");
         settext(event.target.value)
     }
 
-  const [text,settext] = useState("Enter text here");
+  const [text,settext] = useState("");
 //   settext("new text"); //correct way to change the text
 
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lopercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleclearClick}>Clear text</button>
+        <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy text</button>
     </div>
+    <div className="container my-3">
+        <h2>Your text summery</h2>
+        <p>{text.split(" ").length} <b>words ,</b> {text.length} <b>characters</b> </p>
+        <p>{0.008 * text.split(" ").length} <b>Minutes read</b> </p>
+        <h2>preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
